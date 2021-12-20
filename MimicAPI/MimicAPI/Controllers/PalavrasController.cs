3cba9ab3c1c53a128fc64ff606ef9bc74eefff3c
Joselito <junior-joselito@hotmail.com>
2021-12-20 17:49:48 -0300

    Inicio da paginacao

--- MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MimicAPI.Data;
 using MimicAPI.Models;
 using System;
@@ -20,9 +21,21 @@ namespace MimicAPI.Controllers
 
         [Route("")]
         [HttpGet]
-        public ActionResult ObterTodas()
+        public ActionResult ObterTodas(DateTime? data, int? numPagina, int? qtdRegistros)
         {
-            return Ok(_banco.Palavras);
+            var item = _banco.Palavras.AsQueryable();
+
+            if(data.HasValue)
+            {
+                item = item.Where(a => a.Criado > data.Value || a.Atualizado > data.Value);
+            }
+
+            if(numPagina.HasValue)
+            {
+                item = item.Skip((numPagina.Value - 1) * qtdRegistros.Value).Take(qtdRegistros.Value);
+            }
+
+            return Ok(item);
         }
 
         [Route("{id}")]
@@ -43,13 +56,18 @@ namespace MimicAPI.Controllers
         {
             _banco.Palavras.Add(palavra);
             _banco.SaveChanges();
-            return Ok();
+            return Created($"/ApiBehaviorOptions/Palavras/{palavra.Id}", palavra);
         }
 
         [Route("{id}")]
         [HttpPut]
         public ActionResult Atualizar(int id, [FromBody] Palavras palavra)
         {
+            Palavras obj = _banco.Palavras.AsNoTracking().FirstOrDefault(a => a.Id == id);
+
+            if (obj == null)
+                return StatusCode(404);
+
             palavra.Id = id;
             _banco.Palavras.Update(palavra);
             _banco.SaveChanges();
@@ -62,11 +80,14 @@ namespace MimicAPI.Controllers
         {
             Palavras palavra = _banco.Palavras.Find(id);
 
+            if (palavra == null)
+                return StatusCode(404);
+
             palavra.Ativo = false;
             _banco.Palavras.Update(palavra);
             _banco.SaveChanges();
             
-            return Ok();
+            return StatusCode(204);
         }
     }
 }
